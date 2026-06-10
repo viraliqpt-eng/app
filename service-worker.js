5fb0daebb3b25f6c7c06f6acff3d8bbc8e234a5d
viraliqpt-eng <viraliqpt@gmail.com>
2026-06-10 15:03:31 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,11 @@
-const CACHE_NAME = "unwritten-human-v04";
+const CACHE_NAME = "unwritten-human-v05";
 const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
-  self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
